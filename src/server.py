fc608149fe79617114781c66734b3f68c4a048f3
tomasdiazwahl <tomasdiazwahl@gmail.com>
2023-03-27 17:30:36 -0700

    commit server 2

--- src/server.py
+++ src/server.py
@@ -6,27 +6,43 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 
 class Server:
+    # Server constructor
     def __init__(self, host, port):
         self.host = host
         self.port = port
+        # client list will get client socket and client IP address for each client
         self.clients = []
+        self.client_id = 0
+        self.server_socket = None
 
-    def start(self):
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket.bind((self.host, self.port))
-        server_socket.listen()
+    # this method starts the server and opens a TCP connection
+    def start_server(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # setsockopt allows for reuse of socket addresses that are not currently in use
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen()
 
         logging.info(f'Server listening on {self.host}:{self.port}')
 
-        while len(self.clients) < 2:
-            client_socket, client_address = server_socket.accept()
+    def listen_for_clients(self, max_clients):
+        while len(self.clients) < max_clients:
+            client_socket, client_address = self.server_socket.accept()
             logging.info(f'Client {client_address} connected')
             client_thread = threading.Thread(target=self.handle_client, args=(client_socket, client_address))
             client_thread.start()
 
     def handle_client(self, client_socket, client_address):
-        identifier = 'X' if len(self.clients) == 0 else 'Y'
+        if len(self.clients) == 0:
+            identifier = 'X'
+            self.client_id = 0
+        elif len(self.clients) == 1:
+            identifier = 'Y'
+            self.client_id = 1
+        else:
+            self.client_id += 1
+            identifier = self.client_id
+
         self.clients.append((client_socket, client_address))
 
         message = client_socket.recv(1024).decode('utf-8')
@@ -47,4 +63,4 @@ class Server:
 
 if __name__ == '__main__':
     server = Server()
-    server.start()
+    server.start_server()
